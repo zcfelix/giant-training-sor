9e52ac5fa5f99323a8e92cdf9256bd1805baf763
Ying Zhou <ybzhou@thoughtworks.com>
2018-04-10 00:54:11 +0800

    finish docker compose configuration

--- src/main/java/com/thoughtworks/felix/training/picture/interfaces/rest/ImagesApi.java
+++ src/main/java/com/thoughtworks/felix/training/picture/interfaces/rest/ImagesApi.java
@@ -17,10 +17,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
 
